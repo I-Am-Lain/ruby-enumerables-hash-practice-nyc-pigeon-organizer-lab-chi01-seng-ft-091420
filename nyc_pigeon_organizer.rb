0af126d489682164b960753a6d245d486d74cb93
Will Vetter <vetter.william@gmail.com>
2020-09-10 23:49:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,50 +1,14 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
-  new_data  = {}
-
-  data.each do |i, j|
-    j.each do |colour, name|
-      new_data[name] = j
+  data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        binding.pry
+      end
     end
-    puts "Hello World"
   end
-
-  puts new_data.values
-  new_data
-  # new_data = {}
-  #
-  # data.each do |k, v|
-  #   v.each do |c|
-  #     c.each do |n|
-  #       if !new_data[n]
-  #         new_data[n] = {k => c}
-  #       end
-  #     end
-  #   end
-  # end
-  #
-  # puts new_data
-  # new_data.each do |i|
-  #   puts i
-  # end
-
-  # new_data = {
-  #   n => {    # n = names
-  #     k << c.to_str  # k = :color, :gender, :lives
-  #   }              # c = :purple, :grey,.... :male, :female, "Subway", "Central Park"
-  #
-  # }
-
-
-  # new_data = {}
-  #
-  # data.each do |toplevel|
-  #   toplevel.each do |midlevel|
-  #     midlevel.each do |name|
-  #       new_data[name] = toplevel
-  #     end
-  #   end
-  #
-  # end
-  # new_data
+  final_array
+  binding.pry
 end
