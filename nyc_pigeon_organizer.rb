d34df5440efabdfc452c2dab27fe07eafb47b891
Will Vetter <vetter.william@gmail.com>
2020-09-10 23:56:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,8 +6,8 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
       names.each do |name|
         binding.pry
-        if !final_array(name)
-          final_array(name) = {}
+        if !final_array[name]
+          final_array[name] = {}
         end
 
       end
